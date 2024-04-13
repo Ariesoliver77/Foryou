24510aa7a62bbb7dfef636599e5d81cd58b4c73c
AriesOliver <145284374+Ariesoliver77@users.noreply.github.com>
2024-04-13 20:50:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Aaaaa, I like you🥰🥰🥰🥰🥰";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
